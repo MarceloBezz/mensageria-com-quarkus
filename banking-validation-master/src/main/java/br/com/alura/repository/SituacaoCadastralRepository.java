b96f56792d1ec52e0c362249921675cfd3637596
Marcelo <mdbezerra16@gmail.com>
2026-02-24 08:50:07 -0300

    :tada: Início do projeto

---
+++ banking-validation-master/src/main/java/br/com/alura/repository/SituacaoCadastralRepository.java
@@ -0,0 +1,13 @@
+package br.com.alura.repository;
+
+import br.com.alura.domain.Agencia;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class SituacaoCadastralRepository implements PanacheRepository<Agencia> {
+
+    public Agencia findByCnpj(String cnpj) {
+        return find("cnpj", cnpj).firstResult();
+    }
+}
